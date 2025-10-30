29dd81966e0d7044dd5b095ad081400e6f0e87b0
GinaXYZ <qiiina122@hotmail.de>
2025-10-30 12:16:21 +0100

    Initial commit for TestAPI

---
+++ TestAPI/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace TestAPI
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ TestAPI/Program.cs
@@ -0,0 +1,72 @@
+using WeatherUtils;
+using Microsoft.AspNetCore.Builder;
+namespace TestAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            var summaries = new[]
+            {
+                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            };
+            // Anonyme Methode als Endpunkt
+            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                    new WeatherForecast
+                    {
+                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                        TemperatureC = Random.Shared.Next(-20, 55),
+                        Summary = summaries[Random.Shared.Next(summaries.Length)]
+                    })
+                    .ToArray();
+                return forecast;
+            })
+            .WithName("GetWeatherForecast")
+            .WithOpenApi();
+
+            // Benannte Methode als Endpunkt
+
+
+            app.Run();
+        }
+
+        public static Person GetPersonByID(int id, int mul)
+        {
+            return new Person
+            {
+                ID = id * mul,
+                FirstName = "John",
+                LastName = "Doe"
+            };
+        }
+
+            public static double GetDewPoint(double num, double num2, double num3)
+            {
+            return num2 * num3 / (num - num3);
+            }
+        }
+    }
